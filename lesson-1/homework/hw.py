ac18c74becb99316498b875b476f54ccd8974875
Azizbek3660 <azizazizbek314@gmail.com>
2025-06-11 20:42:13 +0500

    Create hw.py

---
+++ lesson-1/homework/hw.py
@@ -0,0 +1,25 @@
+1
+eni=int(input(('enter eni')))
+boyi=int(input('enter boyi'))
+p=2*(eni+boyi)
+s=eni*boyi
+print(p,s)
+2
+
+diametr=int(input(('enter diametr')))
+p=int(3.14)
+L=p*diametr
+L
+3
+a=int(input(('enter a')))
+b=int(input(('enter b')))
+ortacha=(a+b)/2
+ortacha
+4
+a=int(input(('enter a')))
+b=int(input(('enter b')))
+sum=(a+b)
+kopaytma=(a*b)
+akvadrat=a**2
+bkvadrat=b**2
+print(sum,kopaytma,akvadrat,bkvadrat)
